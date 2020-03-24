13960811f81a196d2c477f62d7cd63295d89f169
Jieun Kang <jieunkang101@gmail.com>
2020-03-24 14:17:55 -0700

    add Account/Register.cshtml and Login.cshtml files and update AccountController.cs

--- LibraryCatalog/ViewModels/LoginViewModel.cs
+++ LibraryCatalog/ViewModels/LoginViewModel.cs
@@ -1,8 +1,8 @@
 namespace LibraryCatalog.ViewModels
 {
-    public class LoginViewModel
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
+  public class LoginViewModel
+  {
+    public string Email { get; set; }
+    public string Password { get; set; }
+  }
 }
